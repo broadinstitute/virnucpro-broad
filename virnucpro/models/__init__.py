393961101466a426a37a30762cc252103f2316f6
Cesar Arze <cesar.arze@gmail.com>
2026-02-03 15:29:31 -0500

    feat(06-02): export packed attention utilities from models module
    - Add imports: create_position_ids_packed, flash_attn_varlen_wrapper, FLASH_ATTN_AVAILABLE
    - Update __all__ list with new exports
    - Maintain existing ESM2 exports

--- virnucpro/models/__init__.py
+++ virnucpro/models/__init__.py
@@ -4,8 +4,16 @@ from virnucpro.models.esm2_flash import (
     ESM2WithFlashAttention,
     load_esm2_model
 )
+from virnucpro.models.packed_attention import (
+    create_position_ids_packed,
+    flash_attn_varlen_wrapper,
+    FLASH_ATTN_AVAILABLE
+)
 
 __all__ = [
     'ESM2WithFlashAttention',
-    'load_esm2_model'
+    'load_esm2_model',
+    'create_position_ids_packed',
+    'flash_attn_varlen_wrapper',
+    'FLASH_ATTN_AVAILABLE'
 ]
